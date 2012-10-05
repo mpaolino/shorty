67e786cd0a0bed47aa8a18ab31e4fb6eeae46f9d
Miguel Paolino <mpaolino@gmail.com>
2012-10-05 20:11:24 -0300

    Prefixed all api URL paths with '/api/'

--- shorty/routes.py
+++ shorty/routes.py
@@ -7,6 +7,6 @@ url = app.add_url_rule
 
 url('/', 'index', index)
 url('/<encoded>', 'decode', decode, methods=['GET', 'POST'])
-url('/register', 'register', url_register, methods=['POST'])
-url('/reports', 'reports', reports, methods=['POST'])
-url('/getqr', 'generateqr', generateqr, methods=['GET', 'POST'])
+url('/api/register', 'register', url_register, methods=['POST'])
+url('/api/reports', 'reports', reports, methods=['POST'])
+url('/api/getqr', 'generateqr', generateqr, methods=['GET', 'POST'])
